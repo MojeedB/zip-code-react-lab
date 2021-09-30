98dca8e06b47509cd5a2420032d286fe6a79ab40
MojeedB <mojeedbello101@yahoo.com>
2021-09-29 20:30:38 -0400

    Zip-Search Attempt

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -3,25 +3,71 @@ import './App.css';
 
 
 function City(props) {
-  return (<div>This is the City component</div>);
+ 
+  return (
+    <div className="city-container">
+      <div className="header">{props.locationText}</div>
+      <div className="content">
+        <ul>
+          <li>State: {props.stateName}</li>
+          <li>Location: {props.location}</li>
+          <li>Population (estimated): {props.population}</li>
+          <li>Total Wages: {props.wages}</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 function ZipSearchField(props) {
-  return (<div>This is the ZipSearchField component</div>);
+  return (
+    <div className="zip-search-field">
+      <b> Zip Code:</b>
+      <input type="text" placeholder="Try 10016" onChange=
+        { props.changeHandler }/> 
+    </div>
+  );
 }
 
 
 class App extends Component {
+
+  state = {
+    zipCode: '',
+    cities: [],
+  }
+
+  zipChanged = (event) => {
+    //console.log(event.target.value);
+    this.setState( {zipCode: event.target.value} )
+
+    if(event.target.value.length > 4){
+      fetch("http://ctp-zip-api.herokuapp.com/zip/" + event.target.value)
+      .then((response) => response.json())
+      .then(this.saveCities)
+      //.then((data) => console.log(data))
+      // .then((cities) => this.setState(cities))
+    }
+  }
+
+  saveCities = (cities) => {
+    this.setState({cities})
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Zip Code Search</h2>
         </div>
-        <ZipSearchField />
+        <ZipSearchField zipCode = { this.state.zipCode } 
+          changeHandler = { this.zipChanged }/>
         <div>
-          <City />
-          <City />
+          { this.state.cities.map((city) => <City stateName = {city.State} 
+            locationText = {city.LocationText} 
+            location = {"("+ city.Lat + "," + city.Long + ")"}
+            population = {city.EstimatedPopulation} wages = {city.TotalWages}
+            />) }
         </div>
       </div>
     );
